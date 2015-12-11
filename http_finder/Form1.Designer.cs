7ffd401d4aca6c49bc2e1474161e2d8a25f7b6bc
Eyup CELIK <info@eyupcelik.com.tr>
2015-12-11 22:32:06 +0200

    initial commit

---
+++ http_finder/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace http_finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtFound = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGetHtml = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtTarget.Location = new System.Drawing.Point(12, 26);
+            this.txtTarget.Multiline = true;
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(170, 271);
+            this.txtTarget.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(206, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(104, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtFound
+            // 
+            this.txtFound.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFound.Location = new System.Drawing.Point(347, 26);
+            this.txtFound.Multiline = true;
+            this.txtFound.Name = "txtFound";
+            this.txtFound.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFound.Size = new System.Drawing.Size(170, 271);
+            this.txtFound.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Target";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Found";
+            // 
+            // btnGetHtml
+            // 
+            this.btnGetHtml.Location = new System.Drawing.Point(206, 55);
+            this.btnGetHtml.Name = "btnGetHtml";
+            this.btnGetHtml.Size = new System.Drawing.Size(104, 23);
+            this.btnGetHtml.TabIndex = 6;
+            this.btnGetHtml.Text = "Get HTML";
+            this.btnGetHtml.UseVisualStyleBackColor = true;
+            this.btnGetHtml.Click += new System.EventHandler(this.btnGetHtml_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(12, 318);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtStatus.Size = new System.Drawing.Size(505, 142);
+            this.txtStatus.TabIndex = 7;
+            this.txtStatus.TextChanged += new System.EventHandler(this.txtStatus_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 472);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnGetHtml);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFound);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTarget);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Http Founder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtFound;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetHtml;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ http_finder/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace http_finder
+{
+    public partial class Form1 : Form
+    {
+        readonly List<Task> _taskList = new List<Task>(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(HttpScan);
+            Task.Factory.StartNew(HttpsScan);
+        }
+        private void btnGetHtml_Click(object sender, EventArgs e)
+        {
+            if (_taskList.Count > 0)
+            {
+                for (var i = 0; i < _taskList.Count; i++)
+                {
+                    if (_taskList[i].IsCompleted == false)
+                    {
+                        _taskList[i].Wait();
+                    }
+                    else
+                    {
+                        _taskList.Clear();
+                    }
+                }
+            }
+            Task.Factory.StartNew(HttpRequest);
+            Task.Factory.StartNew(HttpsRequest);
+        }
+
+        private void HttpScan()
+        {
+            const int finOctet = 255;
+            const int firstOctet = 1;
+            var ip = txtTarget.Text.Trim();
+            const int port1 = 80;
+            try
+            {
+                var parse = ip.Split('.');
+                for (var i = firstOctet; i < finOctet; i++)
+                {
+                    var status = $"{parse[0]}.{parse[1]}.{parse[2]}.{i}{":"}{port1}";
+                    var t1 = $"{parse[0]}.{parse[1]}.{parse[2]}.{i}";
+                    txtStatus.Text += $"{Environment.NewLine}{status} için tarama işlemi başladı.";
+                    _taskList.Add(Task.Factory.StartNew(() => ConnectTcp(t1, port1)));
+                }
+            }
+            catch (Exception)
+            {
+                //todo:dasda
+            }
+            finally
+            {
+                if (_taskList.Count > 0)
+                {
+                    for (var i = 0; i < _taskList.Count; i++)
+                    {
+                        if (_taskList[i].IsCompleted == false)
+                        {
+                            _taskList[i].Wait();
+                        }
+                        else
+                        {
+                            txtStatus.Text += Environment.NewLine + @"HTTP servisi tespit işlemi tamamlandı...";
+                        }
+                    }
+                }
+            }
+        }
+        private void HttpsScan()
+        {
+            const int finOctet = 255;
+            const int firstOctet = 1;
+            var ip = txtTarget.Text.Trim();
+            const int port2 = 443;
+            try
+            {
+                var parse = ip.Split('.');
+                for (var i = firstOctet; i < finOctet; i++)
+                {
+                    var status = $"{parse[0]}.{parse[1]}.{parse[2]}.{i}{":"}{port2}";
+                    var t1 = $"{parse[0]}.{parse[1]}.{parse[2]}.{i}";
+                    txtStatus.Text += $"{Environment.NewLine}{status} için tarama işlemi başladı.";
+                    _taskList.Add(Task.Factory.StartNew(() => ConnectTcp(t1, port2)));
+                }
+            }
+            catch (Exception)
+            {
+                //todo:dasda
+            }
+            finally
+            {
+                if (_taskList.Count > 0)
+                {
+                    for (var i = 0; i < _taskList.Count; i++)
+                    {
+                        if (_taskList[i].IsCompleted == false)
+                        {
+                            _taskList[i].Wait();
+                        }
+                        else
+                        {
+                            txtStatus.Text += Environment.NewLine + @"HTTPS servisi tespit işlemi tamamlandı...";
+                        }
+                    }
+                }
+            }
+        }
+        private void ConnectTcp(string t1, int port1)
+        {
+            var client = new TcpClient();
+            try
+            {
+                client.SendTimeout = 1000;
+                client.Connect(t1, port1);
+                txtFound.Text += $"{Environment.NewLine}{t1}:{port1}";
+                client.Close();
+            }
+            catch (SocketException)
+            {
+                //todo:sdasd
+            }
+            finally
+            {
+                GetThreadControl(t1+":"+port1);
+            }
+        }
+
+        private void HttpRequest()
+        {
+            var urlList = new List<string>();
+            var parseUrlFirst = txtFound.Text.Trim().Split('\n');
+            for (var i = 0; i < parseUrlFirst.Count(); i++)
+            {
+                if (parseUrlFirst[i].Replace("\r", "").Contains("80"))
+                {
+                    var ip = parseUrlFirst[i].Replace("\r","").Split(':');
+                    urlList.Add(ip[0]);
+                }
+            }
+            foreach (var t in urlList)
+            {
+                var url = $"{"http://"}{t}";
+                txtStatus.Text += Environment.NewLine + url + @" için http sorgusu başladı.";
+                var uri = new Uri(url);
+                _taskList.Add(Task.Factory.StartNew(() => HttpConnect(url, uri, false)));
+            }
+        }
+        private void HttpsRequest()
+        {
+            var urlList = new List<string>();
+            var parseUrlFirst = txtFound.Text.Trim().Split('\n');
+            for (var i = 0; i < parseUrlFirst.Count(); i++)
+            {
+                if (parseUrlFirst[i].Replace("\r", "").Contains("443"))
+                {
+                    var ip = parseUrlFirst[i].Replace("\r", "").Split(':');
+                    urlList.Add(ip[0]);
+                }
+            }
+            foreach (var t in urlList)
+            {
+                var url = $"{"https://"}{t}";
+                txtStatus.Text += Environment.NewLine + url + @" için https sorgusu başladı.";
+                var uri = new Uri(url);
+                _taskList.Add(Task.Factory.StartNew(() => HttpConnect(url, uri, true)));
+            }
+        }
+        private void HttpConnect(string url, Uri uri, bool isHttps)
+        {
+            try
+            {
+                if (!isHttps)
+                {
+                    var documentName = url.Replace("http://", "");
+                    var saveLocation = Application.StartupPath + @"\Scanner\Saved\" + documentName + ".html";
+                    var request = (HttpWebRequest) WebRequest.Create(uri);
+                    request.UserAgent = "Googlebot/2.1 (+http://www.adeosecurity.com.tr)";
+                    request.Referer = url;
+                    request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                    request.AllowAutoRedirect = false;
+                    request.Headers[HttpRequestHeader.AcceptEncoding] = "sdch";
+                    request.Headers[HttpRequestHeader.AcceptLanguage] = "tr-TR,tr;q=0.8,en-US;q=0.6,en;q=0.4";
+                    try
+                    {
+                        using (var response = request.GetResponse())
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                var htmlsource = reader.ReadToEnd();
+                                response.Close();
+                                reader.Close();
+                                var createHtmlCopy = new StreamWriter(saveLocation);
+                                createHtmlCopy.Write(htmlsource);
+                                createHtmlCopy.Flush();
+                                createHtmlCopy.Close();
+                                txtStatus.Text += Environment.NewLine + url + @" için " + documentName +
+                                                  @".html dosyası kaydedildi";
+                            }
+                        }
+                    }
+                    catch (Exception exp)
+                    {
+                        txtStatus.Text += Environment.NewLine + @"İşlem sırasında bir hata oluştu. Oluşan hata: " +
+                                          exp.Message;
+                    }
+                }
+                else
+                {
+                    var documentName = url.Replace("https://", "");
+                    var saveLocation = Application.StartupPath + @"\Scanner\Saved\" + documentName + ".html";
+                    var request = (HttpWebRequest) WebRequest.Create(uri);
+                    request.UserAgent = "Googlebot/2.1 (+http://www.adeosecurity.com.tr)";
+                    request.Referer = url;
+                    request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                    request.AllowAutoRedirect = false;
+                    request.Headers[HttpRequestHeader.AcceptEncoding] = "sdch";
+                    request.Headers[HttpRequestHeader.AcceptLanguage] = "tr-TR,tr;q=0.8,en-US;q=0.6,en;q=0.4";
+
+                    request.AllowWriteStreamBuffering = true;
+                    request.ProtocolVersion = HttpVersion.Version11;
+                    ServicePointManager.ServerCertificateValidationCallback +=
+                        new RemoteCertificateValidationCallback(ValidateServerCertificate);
+                    try
+                    {
+                        using (var response = request.GetResponse())
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                var htmlsource = reader.ReadToEnd();
+                                response.Close();
+                                reader.Close();
+                                var createHtmlCopy = new StreamWriter(saveLocation);
+                                createHtmlCopy.Write(htmlsource);
+                                createHtmlCopy.Flush();
+                                createHtmlCopy.Close();
+                                txtStatus.Text += Environment.NewLine + url + @" için " + documentName +
+                                                  @".html dosyası kaydedildi";
+                            }
+                        }
+                    }
+                    catch (Exception exp)
+                    {
+                        txtStatus.Text += Environment.NewLine + @"İşlem sırasında bir hata oluştu. Oluşan hata: " +
+                                          exp.Message;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (_taskList.Count > 0)
+                {
+                    for (var i = 0; i < _taskList.Count; i++)
+                    {
+                        if (_taskList[i].IsCompleted == false)
+                        {
+                            _taskList[i].Wait();
+                        }
+                        else
+                        {
+                            txtStatus.Text += Environment.NewLine + @"Indirme işlemi tamamlandı...";
+                            _taskList.Clear();
+                        }
+                    }
+                }
+            }
+        }
+        private static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+        private void txtStatus_TextChanged(object sender, EventArgs e)
+        {
+            txtStatus.SelectionStart = txtStatus.Text.Length;
+            txtStatus.ScrollToCaret();
+        }
+        private void GetThreadControl(string senderIp)
+        {
+            if (_taskList.Count > 0)
+            {
+                for (var i = 0; i < _taskList.Count; i++)
+                {
+                    try
+                    {
+                        while (_taskList[i].IsCompleted)
+                        {
+                            _taskList.RemoveAt(_taskList[i].Id);
+                            txtStatus.Text += Environment.NewLine + senderIp + @" için tarama tamamlandı.";
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        //todo
+                    }
+                }
+            }
+        }
+    }
+}
